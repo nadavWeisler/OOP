12af7c2cebc9e11878d9802fea873fd90dc12469
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2019-12-28 17:15:23 +0200

    Do README

--- ex5/src/filesprocessing/DirectoryProcessor.java
+++ ex5/src/filesprocessing/DirectoryProcessor.java
@@ -1,8 +1,9 @@
 package filesprocessing;
 
-import filesprocessing.Exceptions.ErrorException.ErrorException;
-import filesprocessing.Exceptions.ErrorException.IOProblemException;
-import filesprocessing.Exceptions.ErrorException.InvalidUsageException;
+import filesprocessing.Exceptions.ErrorException.*;
+import filesprocessing.filterPackage.Filter;
+import filesprocessing.filterPackage.FilterFactory;
+import filesprocessing.orderPackage.OrderFactory;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -28,12 +29,71 @@ public class DirectoryProcessor {
     }
 
     /**
+     * Create section array list from file
+     *
+     * @param fileName File name
+     * @return Section array list
+     * @throws ErrorException Error
+     */
+    public static ArrayList<Section> CreateSectionsFromFileName(String fileName) throws ErrorException {
+        ArrayList<Section> result = new ArrayList<>();
+        ArrayList<String> fileList = Utils.fileToArrayList(fileName);
+        Filter newFilter = null;
+        boolean doneOrder = true;
+        boolean inFilter = false;
+        boolean inOrder = false;
+        for (int i = 0; i < fileList.size(); i++) {
+            if (fileList.get(i).equals(Utils.FILTER_SECTION)) {
+                if (inOrder) {
+                    inFilter = true;
+                    inOrder = false;
+                    result.add(new Section(newFilter, OrderFactory.CreateOrder("", i)));
+                } else if (inFilter) {
+                    inFilter = false;
+                    newFilter = FilterFactory.CreateFilter("", i);
+                } else {
+                    if(doneOrder) {
+                        inFilter = true;
+                        doneOrder = false;
+                    } else {
+                        throw new MissedSubSectionException();
+                    }
+                }
+            } else if (fileList.get(i).equals(Utils.ORDER_SECTION)) {
+                if (inFilter) {
+                    newFilter = FilterFactory.CreateFilter("", i);
+                    inFilter = false;
+                    inOrder = true;
+                } else if (inOrder) {
+                    result.add(new Section(newFilter, OrderFactory.CreateOrder("", i)));
+                    inOrder = false;
+                } else {
+                    inOrder = true;
+                }
+            } else {
+                if (inFilter) {
+                    newFilter = FilterFactory.CreateFilter(fileList.get(i), i);
+                    inFilter = false;
+                } else if (inOrder) {
+                    result.add(new Section(newFilter, OrderFactory.CreateOrder(fileList.get(i), i)));
+                    newFilter = null;
+                    doneOrder = true;
+                } else {
+                    throw new BadFormatException();
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * @param args
      */
     public static void main(String[] args) {
         try {
             TestArgs(args);
-            ArrayList<Section> sections = Parser.CreateSectionsFromFileName(args[1]);
+            ArrayList<Section> sections = CreateSectionsFromFileName(args[1]);
 
             for (Section section : sections) {
                 section.PrintSection(args[0]);
